7601abd00030c7daa0eec24ee29cb130fd58813e
WMXPY <wm@mengw.io>
2019-10-17 14:35:38 -0500

    add simple gcp image

---
+++ gcp/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+CREDENTIAL=$1
+CLUSTER=$2
+ZONE=$3
+PROJECT=$4
+DEPLOYMENT=$5
+CONTAINER=$6
+IMAGE=$7
+
+echo "$CREDENTIAL" > gcp-credential-key.json
+
+gcloud auth activate-service-account --key-file gcp-credential-key.json
+gcloud auth list
+
+gcloud container clusters get-credentials main --zone us-central1-a --project rpntechserver
+
+kubectl set image deployment/$DEPLOYMENT $CONTAINER=$IMAGE
